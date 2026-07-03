75a1bd2daefc95d004b6b8b672dacba86d9a6d67
christinekara1981-code <christinekara1981@gmail.com>
2026-07-03 16:35:31 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+// Paste your deployed Google Apps Script Web App URL between the quotes below.
+window.COURTESY_CAR_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbw_q9C73e2Xm9JXJgud804Iw8c0q2p1qd5Kndu8dv3ltMSI1R4Bxk0YKwSIRZoRBxQX/exec';
